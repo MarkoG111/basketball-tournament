047cbec23fc6ac2a999756b060aacbf43a1364c8
MarkoG111 <markogacanovic111@gmail.com>
2024-08-27 23:45:05 +0200

    Calculating team form

--- task.js
+++ task.js
@@ -17,20 +17,51 @@ function readJSONFile(filePath) {
   })
 }
 
+// Function to calculate the form of a team based on exhibition results
+function calculateTeamForm(exhibitions, allTeamsFIBARanking) {
+  const teamForm = {}
+  
+  Object.keys(exhibitions).forEach(team => {
+    let totalForm = 0
+
+    exhibitions[team].forEach(match => {
+      const opponentFIBARank = allTeamsFIBARanking[match.Opponent]
+      const result = match.Result.split('-').map(Number)
+      const scoreDifference = result[0] - result[1] 
+
+      // Calculate form adjustment: consider win/loss, opponent strength, and score difference
+      if (scoreDifference > 0) { // Team won
+        totalForm += 1 + scoreDifference / 20
+        totalForm += (opponentFIBARank > allTeamsFIBARanking[team] ? 0.5 : -0.5) 
+      } else { // Team lost
+        totalForm += scoreDifference / 20
+        totalForm += (opponentFIBARank < allTeamsFIBARanking[team] ? -0.5 : 0.5)
+      }
+    })
+
+    teamForm[team] = totalForm
+  })
+
+  return teamForm
+}
+
 async function main() {
   try {
     const groupData = await readJSONFile(path.join(__dirname, 'groups.json'))
     const exhibitionData = await readJSONFile(path.join(__dirname, 'exhibitions.json'))
 
-    console.log('Grupna faza - I kolo:')
-    console.log('Grupa A')
-    console.log(groupData['A'][0]['Team'] + ' - ' + groupData['A'][2]['Team'] + ' (85:79)')
-    console.log(groupData['A'][1]['Team'] + ' - ' + groupData['A'][3]['Team'] + ' (92:80)')
-    console.log('\n' + groupData['A'][0]['FIBARanking'] + '-' + groupData['A'][1]['FIBARanking'] + '-' + groupData['A'][2]['FIBARanking'] + '-' + groupData['A'][3]['FIBARanking'])
+    const allTeamsFIBARanking = {} // Object to store teams and their rankings
+    for (const group of Object.values(groupData)) {
+      group.forEach(team => {
+        allTeamsFIBARanking[team.ISOCode] = team.FIBARanking
+      })
+    }
+
+    let teamsForm = calculateTeamForm(exhibitionData, allTeamsFIBARanking)
+    console.log(teamsForm)
   } catch (err) {
     console.error(err)
   }
 }
-
 
 main()
